a346bbb3b12538cbda1dc70a0e42f5c2fa3e9108
forest-211 <767425412lin@gmail.com>
2023-04-25 22:56:04 +0800

    style: 💄 代码风格调整

--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -6,19 +6,19 @@ import cjs from '@rollup/plugin-commonjs';
 const pkgPath = path.resolve(__dirname, '../../packages');
 const distPath = path.resolve(__dirname, '../../dist/ node_modules');
 
-export function resolvePkgPath (pkgName, isDist) {
+export function resolvePkgPath(pkgName, isDist) {
     if (isDist) {
         return `${distPath}/${pkgName}`;
     }
     return `${pkgPath}/${pkgName}`;
 }
 
-export function getPackageJSON (pkgName) {
+export function getPackageJSON(pkgName) {
     const pkg = `${resolvePkgPath(pkgName)}/package.json`;
     const str = fs.readFileSync(pkg, { encoding: 'utf-8' });
     return JSON.parse(str);
 }
 
-export function getBaseRollupPlugins ({ typescript = {} } = {}) {
+export function getBaseRollupPlugins({ typescript = {} } = {}) {
     return [cjs(), ts(typescript)];
 }
