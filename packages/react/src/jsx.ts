46308e3b31b5bb9b1d55bb335aa4073df264490a
forest-211 <767425412lin@gmail.com>
2023-04-25 22:49:52 +0800

    feat: 🎸 实现 jsx 模块

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,66 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+    Type,
+    Key,
+    Ref,
+    Props,
+    ReactElementType,
+    ElementType
+} from 'shared/ReactTypes';
+
+const ReactElement = function (
+    type: Type,
+    key: Key,
+    ref: Ref,
+    props: Props
+): ReactElementType {
+    const element = {
+        $$typeof: REACT_ELEMENT_TYPE,
+        type,
+        key,
+        ref,
+        props,
+        __mark: 'forest'
+    };
+
+    return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...children: any) => {
+    let key: Key = null;
+    let ref: Ref = null;
+    const props: Props = {};
+
+    for (const prop of config) {
+        const val = config[prop];
+        if (prop === 'key') {
+            if (val !== undefined) {
+                key = `${val}`;
+            }
+            continue;
+        }
+        if (prop === 'ref') {
+            if (val !== undefined) {
+                ref = val;
+            }
+            continue;
+        }
+
+        if ({}.hasOwnProperty.call(config, prop)) {
+            props[prop] = val;
+        }
+    }
+
+    const childrenLength = children.length;
+    if (childrenLength) {
+        if (childrenLength === 1) {
+            props.children = children[0];
+        } else {
+            props.children = children;
+        }
+    }
+
+    return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
---
+++ scripts/rollup/utils.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import ts from 'rollup-plugin-typescript2';
+import cjs from '@rollup/plugin-commonjs';
+
+const pkgPath = path.resolve(__dirname, '../../packages');
+const distPath = path.resolve(__dirname, '../../dist/ node_modules');
+
+export function resolvePkgPath (pkgName, isDist) {
+    if (isDist) {
+        return `${distPath}/${pkgName}`;
+    }
+    return `${pkgPath}/${pkgName}`;
+}
+
+export function getPackageJSON (pkgName) {
+    const pkg = `${resolvePkgPath(pkgName)}/package.json`;
+    const str = fs.readFileSync(pkg, { encoding: 'utf-8' });
+    return JSON.parse(str);
+}
+
+export function getBaseRollupPlugins ({ typescript = {} } = {}) {
+    return [cjs(), ts(typescript)];
+}
