9d3fecf171a8c49b4855d83497d9ddc3f34a60d2
forest-211 <767425412lin@gmail.com>
2023-05-15 21:54:45 +0800

    feat: 🎸 如何触发更新

--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -1,14 +1,15 @@
 import { beginWork } from './beginWork';
 import { completeWork } from './completeWork';
-import { FiberNode } from './fiber';
+import { FiberNode, FiberRootNode, createWorkInProgress } from './fiber';
+import { HostRoot } from './workTags';
 
 let workInProgress: FiberNode | null = null;
 
-function prepareFreshStack(fiber: FiberNode) {
-    workInProgress = fiber;
+function prepareFreshStack(root: FiberRootNode) {
+    workInProgress = createWorkInProgress(root.current, {});
 }
 
-function renderRoor(root: FiberNode) {
+function renderRoot(root: FiberRootNode) {
     // 初始化
     prepareFreshStack(root);
 
@@ -37,6 +38,27 @@ function preformUnitOfWork(fiber: FiberNode) {
     } else {
         workInProgress = next;
     }
+}
+
+export function scheduleUpdateOnFiber(fiber: FiberNode) {
+    // 调度功能
+    // fiberRootNode
+    const root = markUpdateFromFiberToRoot(fiber);
+    renderRoot(root);
+}
+
+export function markUpdateFromFiberToRoot(fiber: FiberNode) {
+    let node = fiber;
+    let parent = node.return;
+    while (parent !== null) {
+        node = parent;
+        parent = node.return;
+    }
+
+    if (node.tag === HostRoot) {
+        return node.stateNode;
+    }
+    return null;
 }
 
 function completeUnitOfWork(fiber: FiberNode) {
