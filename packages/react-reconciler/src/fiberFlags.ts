1803eccd12d2f4d475d73b5ebebface7cea2fa77
forest-211 <767425412lin@gmail.com>
2023-05-30 08:13:37 +0800

    feat: 🎸 实现 commit 阶段

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -4,3 +4,5 @@ export const NoFlags = 0b000001;
 export const Placement = 0b000010;
 export const Update = 0b000100;
 export const ChildDeletion = 0b001000;
+
+export const MutationMask = Placement | Update | ChildDeletion;
--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -1,6 +1,7 @@
 import { beginWork } from './beginWork';
 import { completeWork } from './completeWork';
 import { FiberNode, FiberRootNode, createWorkInProgress } from './fiber';
+import { MutationMask, NoFlags } from './fiberFlags';
 import { HostRoot } from './workTags';
 
 let workInProgress: FiberNode | null = null;
@@ -27,6 +28,35 @@ function renderRoot(root: FiberRootNode) {
     const finishedWork = root.current.alternate;
     root.finishedWork = finishedWork;
     commitRoot(root);
+}
+
+function commitRoot(root: FiberRootNode) {
+    const finishedWork = root.finishedWork;
+
+    if (finishedWork === null) {
+        return;
+    }
+
+    if (__DEV__) {
+        console.warn('commit 阶段开始', finishedWork);
+    }
+
+    // 重置操作
+    root.finishedWork = null;
+
+    // 判断是否存在3个子阶段需要执行的操作
+    const subtreeHasEffect =
+        (finishedWork.subtreeFlags & MutationMask) !== NoFlags;
+    const rootHasEffect = (finishedWork.flags & MutationMask) !== NoFlags;
+
+    if (subtreeHasEffect || rootHasEffect) {
+        // beforeMutation
+        // mutation placement
+        root.current = finishedWork;
+        // layout
+    } else {
+        root.current = finishedWork;
+    }
 }
 
 function workLoop() {
